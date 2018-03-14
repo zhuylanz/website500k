4eb9aba29c1825dcf61ccb63b5aef86332e29fdf
nuhula-asus <zhuylanz@gmail.com>
2018-03-14 19:12:19 +0700

    change port

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.get('/profile', (req, res) => {
 	res.sendFile(__dirname + '/view/profile.html');
 });
 
-app.listen(3214, () => console.log('Web Server Running on 3214'));
+app.listen(8000, () => console.log('Web Server Running on 3214'));
 socket();
