9f9baeecca70800e0f0ea86b25677d5672ba6518
Lan Nguyen <zhuylanz@gmail.com>
2018-03-15 10:42:18 +0700

    first time on air eh!

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const socket = require('./app.js');
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/view'));
 
 app.get('/profile', (req, res) => {
-	res.sendFile(__dirname + '/view/craft/profile.html');
+	res.sendFile(__dirname + '/view/profile.html');
 });
 
 app.listen(8002, () => console.log('Web Server Running on 8002'));
